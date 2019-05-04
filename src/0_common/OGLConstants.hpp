d815355a7e3a60a7b57457e0157ba459e06f9cd4
Marek Gawryszewski <marek.gawryszewski@marketlab.pl>
2019-05-04 14:13:15 +0200

    [0_common]
    Common headers extracted to separate folder - CMakeLists updated accordingly.
    Excluded build folder from git.

---
+++ src/0_common/OGLConstants.hpp
@@ -0,0 +1,28 @@
+/************************************************************
+ *
+ * file: OGLConstants.hpp
+ * brief: Common constants
+ *
+ ************************************************************/
+
+#ifndef COMMON_OGLCONSTANTS_HPP
+#define COMMON_OGLCONSTANTS_HPP
+
+namespace OGL {
+namespace Constants {
+    namespace Window {
+        constexpr int Width  = 800;
+        constexpr int Height = 600;
+    }
+
+namespace Glut {
+    namespace Version {
+        const int Major = 3;
+        constexpr int Minor = 0;
+    }
+}
+
+}
+}
+
+#endif // COMMON_OGLCONSTANTS_HPP
